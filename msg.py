1d0fdbd5e6d2d5400573c0959ef336a0082220b3
Aayush Agarwal <aayush.agarwal_wu@yahoo.com>
2016-05-18 00:31:19 -0400

    fixed error in line 42 and added copyright info

--- msg.py
+++ msg.py
@@ -1,3 +1,19 @@
+# CK2 Music Script Generator
+# Copyright (C) 2016 Gundam Astraea Type F2
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import argparse;
 
 class msg:
@@ -23,10 +39,10 @@ class msg:
                        + "\tname = \""+song+"\"\n\n"
                        + "\tchance = {\n"
                        + "\t\tmodifier = {\n"
-                       + "\t\t\tfactor = "+self.factor+"\n"
+                       + "\t\t\tfactor = "+str(self.factor)+"\n"
                        + "\t\t}\n"
                        + "\t}\n"
-                       + "}\n"
+                       + "}\n";
             
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate a music script');
